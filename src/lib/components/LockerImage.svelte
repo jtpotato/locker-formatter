d0c53b36d7fc89f3ac3323adda21d8ba89fd8978
Joel <58995538+jtpotato@users.noreply.github.com>
2024-06-01 09:36:09 +1000

    initial commit

---
+++ src/lib/components/LockerImage.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import IconCrop from '~icons/carbon/crop';
+
+	export let src: string;
+	const originalSrc = src;
+
+	let isHovered = false;
+</script>
+
+<div
+	class="m-2 relative"
+	on:mouseenter={() => (isHovered = true)}
+	on:mouseleave={() => (isHovered = false)}
+>
+	<img {src} alt="" />
+	{#if isHovered}
+		<div class="absolute top-0 p-1" id="crop-button">
+			<div class="bg-black/70 rounded-full p-2 text-white border border-white/10">
+				<IconCrop />
+			</div>
+		</div>
+	{/if}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import LockerImage from '$lib/components/LockerImage.svelte';
+
+	let fileInput: HTMLInputElement;
+
+	let images: string[] = [];
+
+	const handleFiles = () => {
+		if (!fileInput.files) return;
+		const imageFiles = Array.from(fileInput.files);
+		images = imageFiles.map((file) => URL.createObjectURL(file));
+
+		console.log(images);
+	};
+</script>
+
+<input type="file" accept="image/*" multiple bind:this={fileInput} on:change={handleFiles} />
+
+<div class="columns-4 gap-0">
+	{#each images as image}
+		<LockerImage src={image} />
+	{/each}
+</div>
